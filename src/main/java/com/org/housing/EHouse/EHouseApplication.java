727465c561eef8e3369636a90f95975cc80a21c1
divyalok <the.divyalok@gmail.com>
2017-06-18 07:08:12 +0530

    Baseline Ehouse

---
+++ src/main/java/com/org/housing/EHouse/EHouseApplication.java
@@ -0,0 +1,12 @@
+package com.org.housing.EHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EHouseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/org/housing/EHouse/EHouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.org.housing.EHouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EHouseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
